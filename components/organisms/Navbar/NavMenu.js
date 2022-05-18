c5164b10e59b4154d8951952aa7750bd00020437
ahmdhndr <achmadhendarsyah7@yahoo.com>
2022-05-18 20:01:47 +0700

    Refactor using atomic designs

---
+++ components/organisms/Navbar/NavMenu.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Link from 'next/link';
+
+export default function NavMenu(props) {
+  const { title, active, href } = props;
+  const classTitle = cx({
+    'nav-link': true,
+    active,
+  });
+  return (
+    <li className="nav-item my-auto">
+      <Link href={href}>
+        <a className={classTitle} aria-current="page">
+          {title}
+        </a>
+      </Link>
+    </li>
+  );
+}
+
+NavMenu.propTypes = {
+  title: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+  href: PropTypes.string,
+};
+
+NavMenu.defaultProps = {
+  active: false,
+  href: '/',
+};
---
+++ components/organisms/Navbar/index.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Auth from './Auth';
+import NavMenu from './NavMenu';
+import ToggleMenu from './ToggleMenu';
+
+export default function Navbar() {
+  return (
+    <section>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <Image src="/icon/logo.svg" width={60} height={60} />
+          </a>
+          <ToggleMenu />
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
+              <NavMenu title="Home" active href="/" />
+              <NavMenu title="Games" />
+              <NavMenu title="Reward" />
+              <NavMenu title="Discover" />
+              <NavMenu title="Global Rank" />
+              <Auth isLogin />
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+}
